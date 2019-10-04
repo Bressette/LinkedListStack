3c36064b41f20fc71e8c85dcbc4f0a5009ea802f
Johnathan Bressette <bressetteprogramming@gmail.com>
2019-10-03 22:57:30 -0400

    Added push function to add values to stack

--- main.c
+++ main.c
@@ -7,8 +7,42 @@ struct node
     struct node *next;
 };
 
+struct node push(struct node *head, int newElement)
+{
+    struct node *newNode;
+
+       struct node *newNode, *temp;
+
+    newNode = (struct node *)malloc(sizeof(struct node));
+    newNode->element = newElement;
+    newNode->next = NULL;
+    temp = head;
+
+    if(head == NULL)
+    {
+        head = newNode;
+    }
+
+    else
+    {
+        while(temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+
+    size++;
+    return head;
+}
+
+
+
 int main()
 {
+    struct node *head = NULL;
+    push(head, 10);
+    push(head, 15);
 
 
     return 0;
